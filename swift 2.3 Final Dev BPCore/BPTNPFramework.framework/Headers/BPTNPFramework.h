3b70df5ebd24f5b840501df21ac95503a11dd0f5
Blumon Pay <blumonpay@gmail.com>
2016-11-07 14:33:45 -0600

    Add files via upload

---
+++ swift 2.3 Final Dev BPCore/BPTNPFramework.framework/Headers/BPTNPFramework.h
@@ -0,0 +1,20 @@
+//
+//  BPTNPFramework.h
+//  BPTNPFramework
+//
+//  Created by Blumon Pay on 10/26/16.
+//  Copyright © 2016 Blumon Pay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "sqlite3.h"
+
+//! Project version number for BPTNPFramework.
+FOUNDATION_EXPORT double BPTNPFrameworkVersionNumber;
+
+//! Project version string for BPTNPFramework.
+FOUNDATION_EXPORT const unsigned char BPTNPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BPTNPFramework/PublicHeader.h>
+
+
